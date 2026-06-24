91fbe7cf4f8822a911eded6cd4fd04a95e544d8c
glorypranavi <24wh1a6620@bvrithyderabad.edu.in>
2026-06-24 13:09:56 +0530

    intro to loops

---
+++ loops_1.java
@@ -0,0 +1,11 @@
+public class loops_1 {
+    public static void main(String[] args) {
+        // working of loops 
+        for(int i=0;i<=10;i++)
+        {
+            // intialization|| condition to terminate
+            System.out.println("Hello Java !");
+
+        }
+    }
+}
