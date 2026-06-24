1a746116a03641ac3526389c336ce106c0c2d238
glorypranavi <24wh1a6620@bvrithyderabad.edu.in>
2026-06-24 13:53:48 +0530

    loops -2

---
+++ Loops_2.java
@@ -0,0 +1,15 @@
+public class Loops_2 {
+    public static void main(String[] args) {
+        //while loop for factorial 
+        int fact=1;
+        int n= 5;
+
+     while (n!=0) { 
+         fact*=n;
+         n--;
+     }
+     System.out.println(fact);
+            
+        }
+    }
+
--- loops_1.java
+++ loops_1.java
@@ -1,4 +1,4 @@
-public class loops_1 {
+public class Loops_1 {
     public static void main(String[] args) {
         // working of loops 
         for(int i=0;i<=10;i++)
