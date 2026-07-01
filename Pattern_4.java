a8735263d60568eb84baf5585cb1fbd8a413c6fd
glorypranavi <24wh1a6620@bvrithyderabad.edu.in>
2026-07-01 13:47:51 +0530

    pattern 4

--- Pattern_4.java
+++ Pattern_4.java
@@ -3,20 +3,20 @@ public class Pattern_4{
     public static void main(String[] args) {
         Scanner sc= new Scanner(System.in);
         int n= sc.nextInt();
-        int num=1;
-        for (int i=0;i<n;i++){
-            for(int j=0;j<=i;j++)
-            {
-                System.out.print(num+" ");
-                
-            }
-            num++;
+        
 
+        for(int i=0;i<n;i++){
+            for(int j=0;j<=n-i;j++)
+            {
+                System.out.print("*");
+            }
             System.out.println();
+        }
+        
         }
             
         }
         
 
-    }
+    
 
