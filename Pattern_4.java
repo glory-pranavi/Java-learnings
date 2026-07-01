da04c6ea4b3af7ee78a0584531d56b1c1cab02d8
glorypranavi <24wh1a6620@bvrithyderabad.edu.in>
2026-07-01 13:16:31 +0530

    pattern 4

---
+++ Pattern_4.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class Pattern_4{
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int n= sc.nextInt();
+        int num=1;
+        for (int i=0;i<n;i++){
+            for(int j=0;j<=i;j++)
+            {
+                System.out.print(num+" ");
+                
+            }
+            num++;
+
+            System.out.println();
+        }
+            
+        }
+        
+
+    }
+
