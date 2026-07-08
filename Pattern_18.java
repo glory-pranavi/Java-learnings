eee62a5e3720b71baf40fef0532baf91f434c31b
glorypranavi <24wh1a6620@bvrithyderabad.edu.in>
2026-07-08 14:28:35 +0530

    Alpha-Triangle Pattern

---
+++ Pattern_18.java
@@ -0,0 +1,20 @@
+public class Pattern_18 {
+    public static void main(String[] args) {
+        int n =3;
+        //char t=s;
+        //char t1=s;
+        for (int i=0;i<n;i++){
+            char s=(char)('A'+n-1-i);//c
+            for(int j=0;j<=i;j++){
+              System.out.print(s);
+              s++;
+              
+              
+            }
+            System.out.println();
+            
+        }
+            
+        }
+    }
+
