717e48f99d3ddb11447d0895e4847e770f27c211
glorypranavi <24wh1a6620@bvrithyderabad.edu.in>
2026-07-08 09:53:17 +0530

    patterns problems

---
+++ Pattern_6.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class Pattern_6{
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int n= sc.nextInt();
+        
+
+        for(int i=0;i<n;i++){
+            int num=1;
+            for(int j=0;j<n-i;j++)
+            {
+                System.out.print(num);
+                num++;
+            }
+
+            System.out.println();
+        }
+        
+        }
+            
+        }
+        
+
+    
