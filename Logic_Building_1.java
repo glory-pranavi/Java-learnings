c0c5da6e5a62eb1a4ed1cc33363014ac6aa1a48f
glorypranavi <24wh1a6620@bvrithyderabad.edu.in>
2026-07-09 20:22:08 +0530

    Digit counting

---
+++ Logic_Building_1.java
@@ -0,0 +1,9 @@
+public class Logic_Building_1 {
+    public static void main(String[] args) {
+        int n=12345;
+        String s=String.valueOf(n);
+        int n1= s.length();
+       System.out.print(n1);
+    }
+    
+}
