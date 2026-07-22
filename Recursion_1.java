85a88d50ec1b87f21d7c2ef53b367674c3728f12
glorypranavi <24wh1a6620@bvrithyderabad.edu.in>
2026-07-22 10:46:48 +0530

    basic recursion

---
+++ Recursion_1.java
@@ -0,0 +1,13 @@
+public class Recursion_1 {
+
+   static  void print(int i,int n){
+        if(i>n) return;
+        System.out.println("JESUS");
+        print(i+1, n);
+    }
+    public static void main(String[] args) {
+        int n=5;
+        print(1,n);
+        
+    }
+}
